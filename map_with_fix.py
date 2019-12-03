9e5ac679b6e766770821b07b329d34fcc98f2250
keith <keith.driver@titania.com>
2019-12-03 19:06:11 +0000

    added ability to generate two data sets

--- map_with_fix.py
+++ map_with_fix.py
@@ -2,7 +2,6 @@ import requests,os,json,time,uuid
 
 directory='.'
 i=1
-session_uuid = str(uuid.uuid1())
 
 for filename in os.listdir(directory):
 	if filename.endswith('log1.json'):
@@ -11,8 +10,6 @@ for filename in os.listdir(directory):
 i=1000
 for report in js:
 	report['nipper_id'] = i
-	report['nipper_session'] = session_uuid
-	report['nipper_text'] = "Research Network second audit" 
 	i = i + 1
 	if  'advisories' in report:
 		if report['advisories'] == ['']:
@@ -48,53 +45,68 @@ for report in js:
 			for g in f:
 				finding = report['findings'][k][g]
 				if isinstance(finding,dict):
-					#print(finding)
 					for item in finding:
 						if item == '':
-							#print("found null")
 							co = finding[item]
 							report['findings'][k][g]["kdd"] = co
 							del report['findings'][k][g][item]
-				#print(report['findings'][k][g])
 							
-skipit ="V-3062"
-def report_it(report):
+def report_it(report,skip):
 	answer = True
 	if  'finding_id' in report:
-		if report['finding_id'] == skipit:
+		if report['finding_id'] == skip:
 			answer = False
-			print("skip it ")
 	return answer
 
-with open("log1_mapped.json","w") as wf:
-	for report in js:
-		if report_it(report) :
-			the_s = json.dumps(report)
-			wf.write(the_s + '\n')
-	wf.close()
-
-with open("10.json","w") as wf:
-	for index in range(0 , 10):
-		if report_it(report) :
-			the_s = json.dumps(js[index])
-			wf.write(the_s + '\n')
-	wf.close()
-
-with open("1000.json","w") as wf:
-	for index in range(0 , 1000):
-		if report_it(report) :
-			the_s = json.dumps(js[index])
-			wf.write(the_s + '\n')
-	wf.close()
-
-with open("2000.json","w") as wf:
-	for index in range(0 , 2000):
-		if report_it(report) :
-			the_s = json.dumps(js[index])
-			wf.write(the_s + '\n')
-	wf.close()
-
-
-with open("log1_formatted.json","w") as wf:
-	json.dump(js, wf, indent=4)
+def dump_files(skip):
+	with open("log1_mapped.json","a") as wf:
+		for report in js:
+			if report_it(report,skip) :
+				the_s = json.dumps(report)
+				wf.write(the_s + '\n')
+		wf.close()
+	with open("10.json","a") as wf:
+		for index in range(0 , 10):
+			if report_it(report,skip) :
+				the_s = json.dumps(js[index])
+				wf.write(the_s + '\n')
+		wf.close()
+	with open("1000.json","a") as wf:
+		for index in range(0 , 1000):
+			if report_it(report,skip) :
+				the_s = json.dumps(js[index])
+				wf.write(the_s + '\n')
+		wf.close()
+	with open("2000.json","a") as wf:
+		for index in range(0 , 2000):
+			if report_it(report,skip) :
+				the_s = json.dumps(js[index])
+				wf.write(the_s + '\n')
+		wf.close()
+	with open("log1_formatted.json","a") as wf:
+		json.dump(js, wf, indent=4)
 	
+def delete_it(filename):
+	if os.path.exists(filename):	
+		os.remove(filename)
+
+delete_it("log1_mapped.json")
+delete_it("10.json")
+delete_it("100.json")
+delete_it("1000.json")
+delete_it("2000.json")
+delete_it("log1_formatted.json")
+
+session_uuid = str(uuid.uuid4())
+print(session_uuid)
+for report in js:
+	report['nipper_session'] = session_uuid
+	report['nipper_text'] = "Research Network first audit" 
+dump_files("")
+session_uuid = str(uuid.uuid4())
+print(session_uuid)
+for report in js:
+	report['nipper_text'] = "Research Network second audit" 
+	report['nipper_session'] = session_uuid
+dump_files("V-3062")
+
