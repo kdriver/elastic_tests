36fb29ec94f9700471624c817bdb478df514ecf3
keith <driver.keith@gmail.com>
2020-01-09 08:00:49 +0000

    Added flag to inhibit fixing and added docker startup for elastic

---
+++ start_elk.bat
@@ -0,0 +1,3 @@
+docker system prune -f
+docker run -d --name myelastic -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.5.1
+docker run --link myelastic:elasticsearch  -p 5601:5601 docker.elastic.co/kibana/kibana:7.5.1
